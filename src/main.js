6366caed92dd43bb92478c1f0bccc733951c9387
dxh_vip <dongxiaohui_vip@163.com>
2023-06-13 14:18:24 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -41,7 +41,7 @@ const main = async () => {
   // 登录
   try {
     await juejin.login(COOKIE)
-
+    console.log('cookie',juejin.login(COOKIE));
     growth.userName = juejin.user.user_name
   } catch {
     throw new Error('登录失败, 请尝试更新 Cookies')
